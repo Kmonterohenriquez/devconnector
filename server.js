f093f7cc414a0960cfb1f4b0207949fe78bd9747
Kevin Montero <kevinwilding1997@gmail.com>
2020-02-22 15:32:02 -0400

    db setup

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('Express');
 const app = express();
 const PORT = process.env.PORT || 5000;
+const connectDB = require('./config/db')
+
+// Connect Database 
+connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
 
