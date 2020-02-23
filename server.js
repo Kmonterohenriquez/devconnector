42ee6f121880f93d8e3274816779899580f48a72
Kevin Montero <kevinwilding1997@gmail.com>
2020-02-23 01:46:48 -0400

    route files with express router setup

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('Express');
 const app = express();
 const PORT = process.env.PORT || 5000;
-const connectDB = require('./config/db')
+const connectDB = require('./config/db');
 
-// Connect Database 
+// Connect Database
 connectDB();
 
 app.get('/', (req, res) => res.send('API Running'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
