707057c02ae8c1e41271351c970eb8664b181c3e
Kevin Montero <kevinwilding1997@gmail.com>
2020-02-24 23:17:27 -0400

    implementing JWT and a custom Auth & JWT verify

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const { check, validationResult } = require('express-validator');
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-
+const config = require('config');
 const User = require('../../models/User');
+const jwt = require('jsonwebtoken');
 // @route POST api/users
 // @desc Register user
 
@@ -56,8 +57,19 @@ router.post(
 			//  Save new user into the DB
 			await user.save();
 
-			// Return jsonwebtoken
-			res.send('User registered');
+			const payload = {
+				user: {
+					id: user.id
+				}
+			};
+
+			jwt.sign(
+				payload,
+				config.get('jwtSecret', { expiresIn: 36000 }, (err, token) => {
+					if (err) throw err;
+					res.json({ token });
+				})
+			);
 		} catch (err) {
 			console.error(err.message);
 			res.status(500).send('Server error');
